047f13a5548abbe9dbd890eb407e9976b25b4abe
Hector Flores <hector.flores@iosoffices.com>
2023-10-13 16:35:36 -0600

    Fix bug on title

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -4,11 +4,11 @@ import styles from "./menu.module.css";
 import { Element } from "../MenuItem";
 import { MenuProps } from "@/types";
 
-export const Menu = ({ data, inline }: MenuProps) => {
+export const Menu = ({ data, inline, title }: MenuProps) => {
   let className = `${styles.menu} ${inline && styles.inline}`;
   return (
     <nav className={className}>
-      {inline && <h2 className={styles.title}>Inventario</h2>}
+      {inline && <h2 className={styles.title}>{title}</h2>}
       {data.map((item) => (
         <Element
           key={item.title}

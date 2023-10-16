93607c96d703ad16da371dfe07d1ad9e97fea229
Hector Martinez <hec.gre81@gmail.com>
2023-10-15 22:53:36 -0600

    Add redirection

--- app/ingredientes/agregar-ingrediente/page.tsx
+++ app/ingredientes/agregar-ingrediente/page.tsx
@@ -1,14 +1,22 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 export default function AgregarIngrediente() {
-  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+  const { register, handleSubmit, formState } = useForm();
+
   const submit = (data: any) => {
     console.log(data);
+    router.push("/ingredientes");
   };
+
   return (
     <form onSubmit={handleSubmit(submit)}>
       <h1>Agregar Ingrediente</h1>
+      {formState.errors?.name && (
+        <p className="error">El nombre del ingrediente es requerido</p>
+      )}
       <input
         {...register("name", { required: true })}
         placeholder="Nombre del Ingrediente"

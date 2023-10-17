ac78fb95f9294f42e9ea4db3acb69088a80847c0
Hector Flores <hec.gre81@gmail.com>
2023-10-17 11:22:53 -0600

    Refactor menu.ts -> menu.json

--- app/ingredientes/layout.tsx
+++ app/ingredientes/layout.tsx
@@ -1,10 +1,16 @@
 import { Menu } from "@/components/Menu";
-import { data } from "./menu";
+import menuData from "./menu.json";
+import { Metadata } from "next";
 const title = "Ingredientes";
+
+export const metadata: Metadata = {
+  title: `Listado de ${title}`,
+  description: "Ingredientes de la aplicación",
+};
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Menu data={data} title={title} inline />
+      <Menu data={menuData} title={title} inline />
       {children}
     </>
   );

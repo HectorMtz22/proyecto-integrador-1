3172ff2271a41e2e37af30a9b305aec9fabda4dd
Hector Martinez <hec.gre81@gmail.com>
2023-10-28 20:59:03 -0600

    Add basic inputs

--- app/platillos/nuevo/page.tsx
+++ app/platillos/nuevo/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import { Dish } from "@/types";
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 export default function AddDishPage() {
-  return <h2>Hello</h2>;
+  const methods = useForm<Dish>();
+  const submit: SubmitHandler<Dish> = (data) => console.log(data);
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(submit)}>
+        <h1>Agregar Platillo</h1>
+        <Input
+          name="name"
+          placeholder="Escribe el nombre del platillo"
+          requiredMessage
+        />
+        <Input
+          type="select"
+          name="temperature"
+          placeholder="Temperatura del Platillo"
+          requiredMessage="Selecciona la temperatura"
+        >
+          <option value="">Selecciona la temperatura</option>
+          {["Caliente", "Frío", "Irrelevante"].map((temperature) => (
+            <option key={temperature} value={temperature}>
+              {temperature}
+            </option>
+          ))}
+        </Input>
+        <Button type="primary">Agregar</Button>
+      </form>
+    </FormProvider>
+  );
 }

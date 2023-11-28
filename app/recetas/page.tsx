65a12e10ace20cf06c63407e08b8febfa8c845e4
Hector Flores <hec.gre81@gmail.com>
2023-11-28 16:06:27 -0600

    Add mocking of recipes

--- app/recetas/page.tsx
+++ app/recetas/page.tsx
@@ -1,4 +1,8 @@
-export default function RecetasPage() {
+import { getRecipes } from "@/services/Recipes";
+
+export default async function RecetasPage() {
+  const data = await getRecipes();
+  console.log(data);
   return (
     <main className="container">
       <h1>Lista de Recetas</h1>
---
+++ services/Recipes.ts
@@ -0,0 +1,22 @@
+const URL = process.env.NEXT_PUBLIC_API_URI;
+
+const MOCK_RECIPES = [
+  {
+    dish: "string",
+    id: 0,
+    ingredients: [
+      {
+        amount: 0,
+        id: 0,
+        name: "string",
+        unit: "string",
+      },
+    ],
+  },
+];
+export const getRecipes = async () => {
+  const response = await fetch(`${URL}/Recipe`);
+  const data: [] = await response.json();
+  if (data.length === 0) return MOCK_RECIPES;
+  return data;
+};

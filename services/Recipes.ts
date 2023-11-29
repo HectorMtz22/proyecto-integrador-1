e21671811e8d2ec360c472f5d7d8aa487cd4ef66
Hector Flores <hec.gre81@gmail.com>
2023-11-29 16:06:12 -0600

    Dont mock recipes

--- services/Recipes.ts
+++ services/Recipes.ts
@@ -2,28 +2,28 @@ import { Recipe } from "@/types";
 
 const URL = process.env.NEXT_PUBLIC_API_URI;
 
-const MOCK_RECIPES: Recipe[] = [
-  {
-    dish: "string",
-    id: 0,
-    ingredients: [
-      {
-        group_id: 0,
-        existence: 0,
-        id: 0,
-        name: "string",
-        unit: "string",
-      },
-    ],
-    count: 3,
-  },
-];
+// const MOCK_RECIPES: Recipe[] = [
+//   {
+//     dish: "string",
+//     id: 0,
+//     ingredients: [
+//       {
+//         group_id: 0,
+//         existence: 0,
+//         id: 0,
+//         name: "string",
+//         unit: "string",
+//       },
+//     ],
+//     count: 3,
+//   },
+// ];
 export const getRecipes = async (): Promise<Recipe[]> => {
   const response = await fetch(`${URL}/Recipe`, {
     cache: "no-store",
   });
   const data: Recipe[] = await response.json();
-  if (data.length === 0) return MOCK_RECIPES;
+  // if (data.length === 0) return MOCK_RECIPES;
   return data.map((recipe) => ({
     ...recipe,
     count: recipe.ingredients.length,

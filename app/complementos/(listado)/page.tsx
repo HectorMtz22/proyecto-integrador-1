5495011f1f8dd8153b106b12d25b0fddeabe4ebf
Hector Martinez <hec.gre81@gmail.com>
2023-11-29 23:47:35 -0600

    Fix styles in complements page

--- app/complementos/(listado)/page.tsx
+++ app/complementos/(listado)/page.tsx
@@ -6,21 +6,25 @@ export default async function Page() {
   const { complements, proteins, sauces, types } = await getComplements();
   return (
     <main className={styles.listado}>
-      <h1>Complementos</h1>
+      <h1>Listado por Tipo de Complemento</h1>
       <section>
         <h2>Complementos</h2>
+        <hr />
         <Complement complement={complements} />
       </section>
       <section>
         <h2>Proteínas</h2>
+        <hr />
         <Complement complement={proteins} />
       </section>
       <section>
         <h2>Salsas</h2>
+        <hr />
         <Complement complement={sauces} />
       </section>
       <section>
         <h2>Tipos</h2>
+        <hr />
         <Complement complement={types} />
       </section>
     </main>
@@ -33,9 +37,9 @@ type ComplementProps = {
 const Complement = ({ complement }: ComplementProps) => {
   return complement.map((item) => {
     return (
-      <div key={item.id}>
-        <h2>{item.name}</h2>
-      </div>
+      <aside key={item.id} className={styles.element}>
+        <p>{item.name}</p>
+      </aside>
     );
   });
 };

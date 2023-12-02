354ae92495e5d3c3ec28d84228db6d87fa304826
Hector Martinez <hec.gre81@gmail.com>
2023-12-02 00:20:02 -0600

    Add button if there arent recipes

--- app/recetas/page.tsx
+++ app/recetas/page.tsx
@@ -1,5 +1,7 @@
+import Button from "@/components/Button";
 import { RecipeItem } from "@/components/RecipeItem";
 import { getRecipes } from "@/services/Recipes";
+import Link from "next/link";
 
 export default async function RecetasPage() {
   const data = await getRecipes();
@@ -7,6 +9,14 @@ export default async function RecetasPage() {
   return (
     <main className="container">
       <h1>Lista de Recetas</h1>
+      {data.length === 0 && (
+        <>
+          <h2>No hay recetas activas</h2>
+          <Link href="/recetas/nueva">
+            <Button type="primary">Agregar Receta</Button>
+          </Link>
+        </>
+      )}
       {data.map((recipe) => (
         <RecipeItem
           key={recipe.id}
